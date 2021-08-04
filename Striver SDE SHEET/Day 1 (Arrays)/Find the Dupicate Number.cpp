24cbe97960b15e41ffdea9a0078e4ae935b534ce
rishabhtyagi2306 <rishabhtyagi.2306@gmail.com>
2021-08-04 22:59:03 +0530

    Added new solution

---
+++ Striver SDE SHEET/Day 1 (Arrays)/Find the Dupicate Number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int n = nums.size(), ans;
+        int fast = nums[0], slow = nums[0];
+        do
+        {
+            fast = nums[nums[fast]];
+            slow = nums[slow];
+        }while(fast != slow);
+        
+        fast = nums[0];
+        while(fast != slow)
+        {
+            fast = nums[fast];
+            slow = nums[slow];
+        }
+        
+        return slow;
+    }
+};
