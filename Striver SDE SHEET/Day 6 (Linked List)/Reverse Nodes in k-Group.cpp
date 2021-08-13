c78ac70795bcbf756e98d0f2941ad5000b75add7
rishabhtyagi2306 <rishabhtyagi.2306@gmail.com>
2021-08-13 18:43:32 +0530

    Added new Algo

---
+++ Striver SDE SHEET/Day 6 (Linked List)/Reverse Nodes in k-Group.cpp
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if(head == NULL || k == 1)
+            return head;
+        
+        ListNode *dummy = new ListNode(-1);
+        dummy -> next = head;
+        
+        ListNode *curr = dummy, *prev = dummy, *nex = dummy;
+        int cnt = 0;
+        while(curr -> next != NULL)
+        {
+            curr = curr -> next;
+            cnt++;
+        }
+        
+        while(cnt >= k)
+        {
+            curr = prev -> next;
+            nex = curr -> next;
+            
+            for(int i = 1; i < k; i++)
+            {
+                curr -> next = nex -> next;
+                nex -> next = prev -> next;
+                prev -> next = nex;
+                nex = curr -> next;
+            }
+            prev = curr;
+            cnt -= k;
+        }
+        
+        return dummy -> next;
+    }
+};
